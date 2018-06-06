83cbf5d4f4349f9d9c73fce456aface25a5122ea
Bryant Fukushima <bmfukushima94@gmail.com>
2018-06-06 00:43:07 -0700

    basic design, show key input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,11 @@
-console.log('hello');
+var guLet = [];
+
+	function userInput(e){
+		
+		guLet.push(e.key);
+
+		document.querySelector('#uRight').innerHTML = e.key;
+		document.querySelector('#guess').innerHTML = guLet;
+	}
+
+	document.onkeydown = userInput;
