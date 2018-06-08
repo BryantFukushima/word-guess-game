08cd7765921034c73d0d99d3b2cce24592441e91
Bryant Fukushima <bmfukushima94@gmail.com>
2018-06-07 18:42:43 -0700

    solve for one answer 'zelda'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,49 @@
 var guLet = [];
+var uRight = document.querySelector('#uRight');
+var guess = document.querySelector('#guess');
+var answers = ['zelda' , 'pink'];
+var randomAnswer = Math.floor(Math.random()*answers.length);
+var solWord = [];
+
+	for ( var i = 0; i < answers[randomAnswer].length; i++ ) {
+
+		solWord.push('_');
+	}
+
+	uRight.innerHTML = solWord.join(' ');
 
 	function userInput(e){
-		
-		guLet.push(e.key);
 
-		document.querySelector('#uRight').innerHTML = e.key;
-		document.querySelector('#guess').innerHTML = guLet;
+		var key = e.key;
+
+		if (answers[randomAnswer] == 'zelda') {
+		if (key == 'z') {
+			solWord.splice(0, 1, 'z');
+			uRight.innerHTML = solWord.join(' ');
+		} else if (key == 'e') {
+			solWord.splice(1, 1, 'e');
+			uRight.innerHTML = solWord.join(' ');
+		} else if (key == 'l') {
+			solWord.splice(2, 1, 'l');
+			uRight.innerHTML = solWord.join(' ');
+		} else if (key == 'd') {
+			solWord.splice(3, 1, 'd');
+			uRight.innerHTML = solWord.join(' ');
+		} else if (key == 'a') {
+			solWord.splice(4, 1, 'a');
+			uRight.innerHTML = solWord.join(' ');
+		} else {
+			guLet.push(key);
+			guess.innerHTML = guLet.join(' ');
+		}
+	}
+
+	if (answers[randomAnswer] == 'zelda' && solWord.join('') == 'zelda') {
+		randomAnswer = Math.floor(Math.random()*answers.length);
+	}
+
+
 	}
 
 	document.onkeydown = userInput;
+
